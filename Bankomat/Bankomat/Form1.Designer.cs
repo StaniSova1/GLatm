e5de297454fc8f1aa0cf1fdde9875b8051d0805e
StaniSova1 <stanislav.roba@akademiasovy.sk>
2017-05-24 12:38:04 +0200

    First commit

---
+++ Bankomat/Bankomat/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Bankomat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fdcbc = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fdcbc.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fdcbc
+            // 
+            this.fdcbc.Controls.Add(this.button2);
+            this.fdcbc.Controls.Add(this.button1);
+            this.fdcbc.Controls.Add(this.textBox1);
+            this.fdcbc.Controls.Add(this.label1);
+            this.fdcbc.Location = new System.Drawing.Point(51, 29);
+            this.fdcbc.Name = "fdcbc";
+            this.fdcbc.Size = new System.Drawing.Size(500, 186);
+            this.fdcbc.TabIndex = 0;
+            this.fdcbc.TabStop = false;
+            this.fdcbc.Text = "Bankomat";
+            this.fdcbc.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Card Number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 27);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 259);
+            this.Controls.Add(this.fdcbc);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bankomat";
+            this.fdcbc.ResumeLayout(false);
+            this.fdcbc.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox fdcbc;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
